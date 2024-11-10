74d1032e633b5d8053f286f7921b9e80e50c617c
Reni <103267773+ReniIrinyi@users.noreply.github.com>
2024-11-10 10:29:57 +0100

    Update Model Task

--- src/model/Task.kt
+++ src/model/Task.kt
@@ -1,4 +1,26 @@
 package model
 
-class Task {
+import java.time.LocalDateTime
+
+data class Task (
+    val id: Int,                        // Unique identifier
+    val title: String,                  // Title of the task
+    val priority: String,               // Priority (High, Medium, Low)
+    val createdAt: LocalDateTime,       // Creation date and time
+    var updatedAt: LocalDateTime,       // Last updated date and time
+    val deadline: LocalDateTime,        // Due date
+    var status: String                  // Status (Created, In Progress, Completed)
+) {
+    companion object {
+        private var currentId: Int = 0
+
+        /**
+         * Generates the next unique ID.
+         * @return The next ID as an integer.
+         */
+        fun generateId(): Int {
+            currentId++
+            return currentId
+        }
+    }
 }
